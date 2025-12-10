b70019c747675776de94a94a3c926879034f89f7
Esteban Houillon <122517989+Carlo-Tentacule@users.noreply.github.com>
2025-12-10 22:07:59 +0100

    feat (user backend): routes & functions for logout and update

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -19,3 +19,21 @@ exports.getUsers = async (req, res) => {
   const users = await User.find();
   res.json(users);
 };
+
+exports.logout = async (req, res) => {
+  const { userId } = req.body;
+  await User.findByIdAndUpdate(userId, { isOnline: false, lastSeen: Date.now() });
+  res.json({ message: "Déconnecté" });
+};
+
+exports.update = async (req, res) => {
+  const { userId, newUsername } = req.body;
+
+  const updatedUser = await User.findByIdAndUpdate(
+    userId,
+    { username: newUsername },
+    { new: true }
+  );
+
+  res.json(updatedUser);
+};
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { login, getUsers } = require("../controllers/userController");
+const { login, getUsers, logout, update } = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/login", login);
 router.get("/", getUsers);
+router.post("/logout", logout);
+router.patch("/update", update);
 
 module.exports = router;
