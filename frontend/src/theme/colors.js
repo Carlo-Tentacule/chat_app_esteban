83c2d74ccc2b46ac04899c14598d0076be5b3c80
Esteban Houillon <122517989+Carlo-Tentacule@users.noreply.github.com>
2025-12-10 18:32:57 +0100

    feat (dark & light mode) : add colors

---
+++ frontend/src/theme/colors.js
@@ -0,0 +1,20 @@
+export const COLORS = {
+  light: {
+    background: "#f5f5f5",
+    text: "#000",
+    card: "#fff",
+    input: "#ddd",
+    inputText: "#111",
+    button: "#000",
+    buttonText: "#fff",
+  },
+  dark: {
+    background: "#111",
+    text: "#fff",
+    card: "#222",
+    input: "#222",
+    inputText: "#fff",
+    button: "#fff",
+    buttonText: "#000",
+  },
+};
