381140c11ffd4f7d68579e069b5b4fafb88d14ae
Esteban Houillon <122517989+Carlo-Tentacule@users.noreply.github.com>
2025-12-10 13:56:38 +0100

    feat(login) : User model, route and controller for login

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,16 @@
+const User = require("../models/User");
+
+exports.login = async (req, res) => {
+  const { username } = req.body;
+
+  let user = await User.findOne({ username });
+
+  if (!user) {
+    user = await User.create({ username, isOnline: true });
+  }
+
+  user.isOnline = true;
+  await user.save();
+
+  res.json(user);
+};
