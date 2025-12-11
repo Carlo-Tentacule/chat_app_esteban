b11160e224875c0f3d9005ae018a33a3020098cc
Esteban Houillon <122517989+Carlo-Tentacule@users.noreply.github.com>
2025-12-11 10:07:52 +0100

    feat (socket) : test connexion

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const connectDB = require("./src/config/db");
 const userRoutes = require("./src/routes/userRoutes");
 
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,4 +14,19 @@ connectDB();
 
 app.use("/users", userRoutes);
 
-app.listen(3000, () => console.log("Backend lancé sur port 3000"));
+const server = http.createServer(app);
+const io = new Server(server, {
+    path: "/ws",
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", socket => {
+  console.log("Un client est connecté :", socket.id);
+  socket.emit("hello", "Bienvenue depuis le serveur !");
+  console.log("User-agent:", socket.handshake.headers["user-agent"]);
+});
+
+server.listen(3000, () => console.log("Backend lancé sur port 3000"));
--- frontend/app/chat/[id].tsx
+++ frontend/app/chat/[id].tsx
@@ -1,8 +1,15 @@
 import { Text } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+import { io } from "socket.io-client";
 
 export default function ChatScreen() {
   const { id } = useLocalSearchParams();
+
+  const socket = io("http://localhost:3000", {
+    path: "/ws"
+  });
+
+  socket.on("connect", () => console.log("OK connecté"));
 
   return <Text>Chat avec user {id}</Text>;
 }
